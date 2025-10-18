04ea2cc8aec835cb7f3f272c72d17e2202c653c3
Your Name <you@example.com>
2025-10-18 17:26:19 +0700

    Cai dat MongoDB va Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const app = require("./app");
 const config = require("./app/config");
-// start server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+const MongoDB = require("./app/utils/mongodb.util"); // Import module DB
+
+async function startServer() {
+  try {
+    // Kết nối với MongoDB
+    await MongoDB.connect(config.db.uri);
+    
+    // Bắt đầu server
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  } catch (error) {
+    console.log("Cannot connect to the database!", error);
+    process.exit();
+  }
+}
+
+startServer();
