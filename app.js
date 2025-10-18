43a0e7e5ceaf0d86958e2e1e6647cf9bfeff3cce
Your Name <you@example.com>
2025-10-18 17:20:30 +0700

    Cai dat xu ly loi 404

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const ApiError = require("./app/api-error"); // <<< Sửa đường dẫn ở đây!
+const contactsRouter = require("./app/routes/contact.route"); // Import Routes
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Định nghĩa route cho API
+app.use("/api/contacts", contactsRouter); // <<< Thêm Routes
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
@@ -13,15 +17,11 @@ app.get("/", (req, res) => {
 
 // Xử lý lỗi 404 cho các route không được định nghĩa
 app.use((req, res, next) => {
-    // Code ở đây sẽ chạy khi không có route được định nghĩa nào khớp
-    // với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
 });
 
 // Middleware xử lý lỗi tập trung
-// define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
-    // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
     return res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
     });
